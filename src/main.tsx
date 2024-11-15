4027bc3b27d720fe4a84a35752839dd5a16c2798
Doeunnkimm <doeunnkimm@gmail.com>
2024-11-15 23:05:07 +0900

    useTransition

--- src/main.tsx
+++ src/main.tsx
@@ -3,13 +3,15 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 // import { SyntheticEvent } from './SyntheticEvent/index.tsx';
 // import { Propagation } from './SyntheticEvent/propagation.tsx';
-import { UpdateState } from './UpdateState/UpdateState.tsx';
+// import { UpdateState } from './UpdateState/UpdateState.tsx';
+import { Transition } from './useTransition/Transition.tsx';
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
     {/* <App /> */}
     {/* <SyntheticEvent /> */}
     {/* <Propagation /> */}
-    <UpdateState />
+    {/* <UpdateState /> */}
+    <Transition />
   </React.StrictMode>
 );
---
+++ src/useTransition/Transition.tsx
@@ -0,0 +1,54 @@
+import { useState, useTransition } from 'react';
+
+type Tab = 'about' | 'post' | 'contact';
+
+export const Transition = () => {
+  const [isPending, startTransition] = useTransition();
+  const [tab, setTab] = useState<Tab>('about');
+
+  const handleSelectTab = (nextTab: Tab) => () => {
+    startTransition(() => {
+      setTab(nextTab);
+    });
+    // setTab(nextTab);
+  };
+
+  return (
+    <>
+      <button onClick={handleSelectTab('about')}>ABOUT</button>
+      <button onClick={handleSelectTab('post')}>POST</button>
+      <button onClick={handleSelectTab('contact')}>CONTACT</button>
+      {isPending && '로딩중!!'}
+      {tab === 'about' && <About />}
+      {tab === 'post' && <Post />}
+      {tab === 'contact' && <Contact />}
+    </>
+  );
+};
+
+export const About = () => {
+  return <h1>About</h1>;
+};
+
+// 무거운 연산이 포함된 컴포넌트
+export const Post = () => {
+  const items = Array.from({ length: 1500 }).map((_, i) => (
+    <Slow
+      key={i}
+      index={i}
+    />
+  ));
+
+  return <ul>{items}</ul>;
+};
+
+const Slow = ({ index }: { index: number }) => {
+  let startTime = performance.now();
+  while (performance.now() - startTime < 1) {}
+
+  return <li># ${index + 1}</li>;
+};
+
+export const Contact = () => {
+  return <h1>Contact</h1>;
+};
