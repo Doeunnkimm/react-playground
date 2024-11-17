fe451aea1004e1446973e7a1cb6d6f547f12608b
Doeunnkimm <doeunnkimm@gmail.com>
2024-11-17 19:50:04 +0900

    flushSync

--- src/main.tsx
+++ src/main.tsx
@@ -1,15 +1,17 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
+import { FlushSync } from './flushSync/FlushSync';
 // import { SyntheticEvent } from './SyntheticEvent/index.tsx';
 // import { Propagation } from './SyntheticEvent/propagation.tsx';
-import { UpdateState } from './UpdateState/UpdateState.tsx';
+// import { UpdateState } from './UpdateState/UpdateState.tsx';
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
     {/* <App /> */}
     {/* <SyntheticEvent /> */}
     {/* <Propagation /> */}
-    <UpdateState />
+    {/* <UpdateState /> */}
+    <FlushSync />
   </React.StrictMode>
 );
