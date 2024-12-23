5760238228340f9fdcaf2f6be59ef10cbe34857f
Doeun Kim <doeunnkimm@gmail.com>
2024-12-23 19:56:47 +0900

    clean up function (#9)

--- src/main.tsx
+++ src/main.tsx
@@ -1,19 +1,21 @@
-import React from 'react';
+// import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
-import { UseOverlayExample } from './overlay/useOverlay/UseOverlay';
+import { CleanUpFunction } from './clean-up-function/CleanUpFunction';
+// import { UseOverlayExample } from './overlay/useOverlay/UseOverlay';
 // import { FlushSync } from './flushSync/FlushSync';
 // import { SyntheticEvent } from './SyntheticEvent/index.tsx';
 // import { Propagation } from './SyntheticEvent/propagation.tsx';
 // import { UpdateState } from './UpdateState/UpdateState.tsx';
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
-  <React.StrictMode>
+  <>
     {/* <App /> */}
     {/* <SyntheticEvent /> */}
     {/* <Propagation /> */}
     {/* <UpdateState /> */}
     {/* <FlushSync /> */}
-    <UseOverlayExample />
-  </React.StrictMode>
+    {/* <UseOverlayExample /> */}
+    <CleanUpFunction />
+  </>
 );
