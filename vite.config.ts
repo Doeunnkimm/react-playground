1843f9e70d270ba80ad483a3555201eb39ab45de
Doeunnkimm <doeunnkimm@gmail.com>
2025-04-05 15:35:44 +0900

    chore: vite-tsconfig-paths

--- vite.config.ts
+++ vite.config.ts
@@ -1,14 +1,9 @@
 import { vanillaExtractPlugin } from '@vanilla-extract/vite-plugin';
 import react from '@vitejs/plugin-react-swc';
 import { defineConfig } from 'vite';
+import tsconfigPaths from 'vite-tsconfig-paths';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react(), vanillaExtractPlugin()],
-  resolve: {
-    alias: [
-      { find: '@', replacement: '/src' },
-      { find: 'theme', replacement: './theme.css.ts' },
-    ],
-  },
+  plugins: [tsconfigPaths(), react(), vanillaExtractPlugin()],
 });
